e808e694c5192c49d83fea97083d2ef9d371768b
남궁은선 <nammugmuhg@gmail.com>
2021-11-23 20:23:14 +0900

    [#2] feat: 메인페이지에 카드 넣기

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,22 @@
-
 import styles from "../styles/Home.module.css";
+import Card from "../src/components/cardbox/Card";
+import styled  from "styled-components";
+
+const Cards = styled.div`
+  display : flex;
+  margin-top : 0;
+  justify-content: space-evenly;
+`;
+
 
 export default function Home() {
   return (
-    <div>
-     </div>
+    <Cards>
+      
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Cards>
   );
 }
