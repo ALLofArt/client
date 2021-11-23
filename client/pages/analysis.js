0205d7f9d30e3c41f23514e4fc7efb87bd71ec48
Eunyeol <ney9083@gmail.com>
2021-11-23 14:17:18 +0900

    [#7] feat : 화풍분석 페이지 사용 설명

---
+++ client/pages/analysis.js
@@ -0,0 +1,10 @@
+export default function analysis() {
+    return (
+        <div>
+            <h1>Look for the painter style</h1>
+            <p>내가 그린 그림을 업로드하고,</p>
+            <p> 내 그림이 어떤 유명한 화가의 화풍과 얼마나 유사한지 확인해보세요.</p>
+        </div>
+    );
+}
+
