505ecd639cb5f8bac0277060904ddddbe6a1fd19
Eunyeol <ney9083@gmail.com>
2021-11-25 21:21:22 +0900

    [#9] feat : 분석 결과 높은 순서 5개 데이터 백분율로 변환 및 저장

--- client/pages/analysis.js
+++ client/pages/analysis.js
@@ -1,6 +1,12 @@
 import Upload from "../src/components/Upload";
 import axios from "axios";
-import { Button, Typography, Modal, Box } from "@material-ui/core";
+import {
+  Button,
+  Typography,
+  Modal,
+  Box,
+  CircularProgress,
+} from "@material-ui/core";
 import { Send, Backup } from "@material-ui/icons";
 import { useState } from "react";
 import styled from "styled-components";
@@ -10,12 +16,14 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "30%",
   bgcolor: "background.paper",
   border: "2px solid #fff",
   borderRadius: 20,
   boxShadow: 24,
   p: 4,
+  textAlign: "center",
+  outline: "none",
 };
 
 export default function analysis() {
@@ -24,21 +32,41 @@ export default function analysis() {
   const [isPreviewAvailable, setIsPreviewAvailable] = useState(false); // state to show preview only for images
   const [errorMsg, setErrorMsg] = useState("");
   const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
+  const [sortArr, setSortArr] = useState(null);
+  // const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    console.log("submit");
     try {
       if (file) {
         const formData = new FormData();
         formData.append("file", file);
         setErrorMsg("");
-        await axios.post(`/api/style`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
+        const response = await axios.post(
+          `http://elice-kdt-2nd-team1.koreacentral.cloudapp.azure.com:5000/api/style`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
           },
+        );
+        console.log(response.data);
+        let sortable = [];
+        for (var percent in response.data) {
+          sortable.push([
+            percent,
+            Math.round((response.data[percent] / 1) * 100 * 10) / 10,
+          ]);
+        }
+        sortable.sort(function (a, b) {
+          return b[1] - a[1];
         });
+        sortable = sortable.slice(0, 5);
+        console.log("sortable", sortable);
+        setSortArr(sortable);
       } else {
         setErrorMsg("Please select a file to add.");
         setOpen(true);
@@ -62,8 +90,11 @@ export default function analysis() {
             aria-describedby="modal-modal-description"
           >
             <Box sx={style}>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                {errorMsg}
+              <Typography
+                id="modal-modal-description"
+                sx={{ mt: 2, border: "none" }}
+              >
+                <strong> {errorMsg} </strong>
               </Typography>
             </Box>
           </Modal>
@@ -102,4 +133,5 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   align-content: center;
+  height: 80vh;
 `;
