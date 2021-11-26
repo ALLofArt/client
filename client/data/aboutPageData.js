1c8825fedeeacc158a5d7d4cfe0f1a0fc695b003
남궁은선 <nammugmuhg@gmail.com>
2021-11-26 21:24:38 +0900

    [#17] docs: about페이지 멤버 소개 데이터

--- client/data/aboutPageData.js
+++ client/data/aboutPageData.js
@@ -8,7 +8,9 @@ export const members = [
   {
     name: "임희지",
     role: "AI, Frontend",
-    introduce: "임희지",
+    introduce: [
+      "문제를 해결하기 위해 필요한 기술을 습득하는 걸 좋아합니다.","기술을 빠르게 배우고 적용할 수 있는 힘을 기르고 있습니다. 💪",
+    ],
     frontImg: "",
     backImg: "",
     colors: "red blue #D070FB",
@@ -16,7 +18,11 @@ export const members = [
   {
     name: "김기원",
     role: "Backend",
-    introduce: "김기원",
+    introduce: [
+      "Data Scentist로 일 하다가 개발자로 커리어를 전향했습니다.",
+      "알고리즘과 운영체제를 좋아해 현재는 알고리즘과 운영체제 공부도 열심히 하고 있습니다.",
+      "이렇게 꾸준히 기본기를 다지고, 미래에는 개발자가 더 편하게 개발할 수 있는 오픈소스 라이브러리, 플랫폼을 만들고 싶습니다.",
+    ],
     frontImg: "",
     backImg: "",
     colors: "gold",
@@ -24,7 +30,12 @@ export const members = [
   {
     name: "남궁은선",
     role: "Frontend",
-    introduce: "남궁은선",
+    introduce: [
+      "2021년 올해 처음 프로그래밍을 도전하게 되었고, 프론트 개발자로서의 길을 시작하게되었습니다.",
+      "프론트엔드 분야는 알면 알수록 공부할것이 참 많아서 힘들지만 응용해보면 새로운 결과가 나와서 흥미를 느꼈습니다.",
+      "엘리스에서 다양한 프로젝트를 하며 사람들과 소통하고 같이 협력하는 과정이 즐거웠습니다.",
+      "앞으로도 더 재밌고 의미있는 프로젝트를 위해 열심히 공부하는 개발자가 되고싶습니다.",
+    ],
     frontImg: "",
     backImg: "",
     colors: "#D070FB",
@@ -32,7 +43,7 @@ export const members = [
   {
     name: "남은열",
     role: "Frontend",
-    introduce: "남은열",
+    introduce: ["남은열"],
     frontImg: "",
     backImg: "",
     colors: "#D070FB",
@@ -40,7 +51,11 @@ export const members = [
   {
     name: "서명현",
     role: "PM,",
-    introduce: "서명현",
+    introduce: [
+      "1. 하고 싶은 생각 하나, 그리고 그  생각은 언제든지 바뀔 수 있다는 각오 하나.",
+      "2. 주도적인 삶. 내가 시켜서 하는 나의 일",
+      "3. 미움 받을 용기",
+    ],
     frontImg: "",
     backImg: "",
     colors: "#9eff5a",
@@ -48,7 +63,9 @@ export const members = [
   {
     name: "송성근",
     role: "AI, Backend",
-    introduce: "송성근",
+    introduce: [
+      "동물, 컴퓨터, 만화를 좋아하고 백엔드 위주로 공부하고 있습니다! 잘 부탁드립니다~",
+    ],
     frontImg: "",
     backImg: "",
     colors: "blue gold",
@@ -56,7 +73,7 @@ export const members = [
   {
     name: "김현준",
     role: "Backend",
-    introduce: "김현준",
+    introduce: ["김현준"],
     frontImg: "",
     backImg: "",
     colors: "gold",
