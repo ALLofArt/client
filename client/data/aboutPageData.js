a7a70e1f182b38a8e1a9afeb831e3a18b9eb6353
남궁은선 <nammugmuhg@gmail.com>
2021-11-26 11:38:00 +0900

    [#17] docs: about페이지 멤버 소개 데이터

---
+++ client/data/aboutPageData.js
@@ -0,0 +1,64 @@
+export const frontExample =
+  "https://images-ext-2.discordapp.net/external/_0ymjNXetQH57BtUs0bSzfAQ8gEYCdUpwejezpLtmsA/https/upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Meisje_met_de_parel.jpg/300px-Meisje_met_de_parel.jpg";
+
+export const backExample =
+  "https://images-ext-2.discordapp.net/external/fZW0LjwR-K_8TCrNNyCGxwMfwZuBWxRvh3Ed0yh_uRU/%3Fq%3Dtbn%3AANd9GcSlpEGOnCW_ew102--j2y_-6O1pYhe-3M7muA%26usqp%3DCAU/https/encrypted-tbn0.gstatic.com/images";
+
+export const members = [
+  {
+    name: "임희지",
+    role: "AI, Frontend",
+    introduce: "임희지",
+    frontImg: "",
+    backImg: "",
+    colors: "red blue #D070FB",
+  },
+  {
+    name: "김기원",
+    role: "Backend",
+    introduce: "김기원",
+    frontImg: "",
+    backImg: "",
+    colors: "gold",
+  },
+  {
+    name: "남궁은선",
+    role: "Frontend",
+    introduce: "남궁은선",
+    frontImg: "",
+    backImg: "",
+    colors: "#D070FB",
+  },
+  {
+    name: "남은열",
+    role: "Frontend",
+    introduce: "남은열",
+    frontImg: "",
+    backImg: "",
+    colors: "#D070FB",
+  },
+  {
+    name: "서명현",
+    role: "PM,",
+    introduce: "서명현",
+    frontImg: "",
+    backImg: "",
+    colors: "green",
+  },
+  {
+    name: "송성근",
+    role: "AI, Backend",
+    introduce: "송성근",
+    frontImg: "",
+    backImg: "",
+    colors: "blue gold",
+  },
+  {
+    name: "김현준",
+    role: "Backend",
+    introduce: "김현준",
+    frontImg: "",
+    backImg: "",
+    colors: "gold",
+  },
+];
