195c164a9d87b0cee4af96ac159ab628102b0853
eunsun <nammugmuhg@gmail.com>
2021-11-30 06:15:50 +0900

    [#20] feat: 갤러리페이지 음악 플레이어추가

--- client/pages/gallery.js
+++ client/pages/gallery.js
@@ -1,3 +1,33 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: 5rem;
+  width:100%;
+  text-align:center;
+
+`;
+
+const MarginTop = styled.div`
+  margin-top: 10vh;
+`;
+
+const Audio = styled.div`
+
+`;
+
 export default function Gallery() {
-  return <div>Gallery page입니다</div>;
+  return (
+    <div>
+      <MarginTop />
+      <Title>Gallery</Title>
+      <Audio>
+      <audio controls src="/music/bgm.mp3">
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
+      </Audio>
+
+
+    </div>
+  );
 }
