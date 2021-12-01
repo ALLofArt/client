68b61006754477c3b1efd445c105416ded01c3b1
Eunyeol <ney9083@gmail.com>
2021-12-02 00:53:34 +0900

    [#32] feat : 화가 정보 페이지 이미지 렌더링 및 배치, 네임카드 배치 및 hover 반응 구현

--- client/pages/artists.js
+++ client/pages/artists.js
@@ -1,6 +1,57 @@
 import styled from "styled-components";
 
-export default function Artists(props) {
+export default function Artists() {
+  const artistList = [
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd asdasdasdasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+    ["/images/davinci.jpeg", "Leonardo Da Vinci asdasdsadasd"],
+  ];
   return (
     <main>
       <Container>
@@ -21,20 +72,20 @@ export default function Artists(props) {
           <Hr />
         </SecondContainer>
         <SecondContainer>
-          <GridRow>
-            <h2></h2>
-            <ImageBar>
+          <MainGridRow>
+            {artistList.map((artist) => (
               <PageTeaser>
                 <ImageWrapper>
                   <TeaserImage>
-                    <figure>
-                      <Image src="/images/davinci.jpeg" alt="da vinci" />
-                    </figure>
+                    <Image src={artist[0]} alt="da vinci" />
                   </TeaserImage>
+                  <NameBox>
+                    <Name>{artist[1]}</Name>
+                  </NameBox>
                 </ImageWrapper>
               </PageTeaser>
-            </ImageBar>
-          </GridRow>
+            ))}
+          </MainGridRow>
         </SecondContainer>
       </Container>
     </main>
@@ -49,6 +100,13 @@ const SecondContainer = styled.section`
   padding-bottom: 3.75rem;
   padding-left: 85px;
   padding-right: 85px;
+  @media only screen and (max-width: 64rem) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media only screen and (max-width: 45rem) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `;
 const H1 = styled.h1`
   grid-column: 1 / span 16;
@@ -73,6 +131,18 @@ const SecondDiv = styled.div`
   grid-template-columns: repeat(11, 1fr);
 `;
 
+const MainGridRow = styled.section`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 10px;
+  @media only screen and (max-width: 64rem) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media only screen and (max-width: 45rem) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
 const Markdown = styled.div`
   grid-column: 1 / span 7;
   padding-top: 1.25rem;
@@ -87,36 +157,60 @@ const Hr = styled.hr`
 `;
 
 const PageTeaser = styled.article`
-  grid-column: 1 / span 6;
-`;
-
-const ImageBar = styled.div`
-  grid-column: 1 / span 3;
-  display: grid;
-  align-content: flex-start;
-  align-items: flex-start;
-  grid-template-columns: repeat(6, 1fr);
-  padding-bottom: 1.875rem;
-`;
-
-const ImageWrapper = styled.div`
-  margin-bottom: 1.25rem;
-  overflow: hidden;
+  position: relative;
 `;
 
 const TeaserImage = styled.div`
-  padding-bottom: 100%;
+  width: 100%;
+  height: 100%;
   will-change: transform;
-  transition: transform 0.4s cubic-bezier(0.23, 1, 0.32, 1);
+  transition: transform 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+`;
+
+const NameBox = styled.div`
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+  display: block;
+  background-color: #061c25;
+  padding: 6% 8%;
+  transition: all 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+`;
+
+const Name = styled.span`
+  display: inline-block;
+  color: white;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  font-size: 0.815rem;
+  font-weight: 800;
+  line-height: 1;
+  letter-spacing: 0.08em;
+  text-overflow: ellipsis;
+  transition: transform 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+`;
+const ImageWrapper = styled.div`
+  overflow: hidden;
+  height: 100%;
   :hover {
     cursor: pointer;
-    transform: scale(1.1);
+    ${TeaserImage} {
+      transform: scale(1.1);
+    }
+    ${NameBox} {
+      background-color: #00667f;
+    }
+    ${Name} {
+      padding-left: 10%;
+    }
   }
 `;
 
 const Image = styled.img`
   width: 100%;
   height: 100%;
-  position: absolute;
-  
+  position: relative;
+  margin: 0;
+  padding: 0;
 `;
