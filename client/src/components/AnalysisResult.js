262de1b3589203e4c369d1297ebfb4d220e20041
Heeji <limhizy15@ajou.ac.kr>
2021-12-02 16:30:56 +0900

    [#26] feat: stylize 결과에 갤러리 등록 버튼 추가

--- client/src/components/AnalysisResult.js
+++ client/src/components/AnalysisResult.js
@@ -30,7 +30,7 @@ export default function AnalysisResult({ sortArr }) {
               <span style={{ fontSize: "4em" }}> %</span>
             </strong>
           </div>
-          <div style={{height:"3vw"}}>
+          <div style={{ height: "3vw" }}>
             {result && (
               <h2>
                 당신의 그림은 화가 {sortArr[0][0]}의 화풍과 {sortArr[0][1]}%
--- client/src/components/TransferResult.js
+++ client/src/components/TransferResult.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
+import { Send } from "@material-ui/icons";
+import { Button } from "@material-ui/core";
 
 export default function TransferResult({ result }) {
   console.log(result);
   return (
     <div>
-      <ResultTitle>결과</ResultTitle>
+      {/* <h1>결과입니다! </h1>
+      <h2>다른 사람에게 자랑하고 싶다면 버튼을~</h2>
+      <Button size="large" endIcon={<ArrowCircleRight />} type="button">
+        <strong>요기~</strong>
+      </Button> */}
+      <ResultTitle>
+        결과입니다! <br />
+        다른 사람에게 자랑하고 싶다면 버튼을~
+        <Button size="large" endIcon={<Send />}></Button>
+      </ResultTitle>
       <ResultImg src={result} />
     </div>
   );
