f0d764144c6d31bb6e200e88e8a313d2d4ccebea
eunsun <nammugmuhg@gmail.com>
2021-12-12 03:15:08 +0900

    [#50] style: 메인페이지 선택지 랜덤색상 생성 함수 분리

--- client/src/components/cardbox/Card.js
+++ client/src/components/cardbox/Card.js
@@ -12,27 +12,7 @@ export default function Card({ frontImg, backImg, explain }) {
   const word = useRef();
 
   // Converts integer to hex
-  const colToHex = (c) => {
-    // Hack so colors are bright enough
-    let color = c < 75 ? c + 75 : c;
-    let hex = color.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  };
 
-  // uses colToHex to concatenate
-  // a full 6 digit hex code
-  const rgbToHex = (r, g, b) => {
-    return "#" + colToHex(r) + colToHex(g) + colToHex(b);
-  };
-
-  // Returns three random 0-255 integers
-  const getRandomColor = () => {
-    return rgbToHex(
-      Math.floor(Math.random() * 255),
-      Math.floor(Math.random() * 255),
-      Math.floor(Math.random() * 255),
-    );
-  };
   return (
     <div className={styles.wrapper}>
       <div
