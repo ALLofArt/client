e48fcfb426f6ece34ad4f663211da8e339ba4b9a
eunsun <nammugmuhg@gmail.com>
2021-12-11 19:57:17 +0900

    [#50] docs: 메인페이지 들어갈 사진 추가

--- client/data/mainPageImg.js
+++ client/data/mainPageImg.js
@@ -1,25 +1,11 @@
 export const mainPageImg = [
-"https://images-ext-1.discordapp.net/external/dKVpVDUsCqqngChejbR6aO7ctgy394xNrH56wVz1ICQ/http/artmotion.co.kr/web/upload/NNEditor/20210721/copy-1626854560-860.jpg"
-,
+  "/main/catmarlin.png",
+  "/main/transferedcat.jpeg",
+  "/main/wintertransfered.jpg",
+  "/main/winterframe.png",
 
-"https://images-ext-2.discordapp.net/external/4972Hs2uTKzESHsHr1e2qQkSRfSkxWBxv7cRuK8JG3w/%3Ftype%3Dw2/https/mblogthumb-phinf.pstatic.net/20131002_258/kordilibrary_1380692376821OuusN_PNG/inspired_bei_mondrian_by_manshonyagger-d35kfou.png"
-,
-
-
-"https://images-ext-2.discordapp.net/external/jvYAnaG4SQVzhn65Xht4Z8oUUfnKv_cFhDcO8iH4Ww8/%3Ftype%3Dw800/https/mblogthumb-phinf.pstatic.net/MjAyMDA0MDdfMjE0/MDAxNTg2MjY0ODAyMjQ5.M5o7fkaIxQnvmg9kTglUEo-whUBQzYSLa97_-ObikZ4g.6wMnQOYmj9XIeDb8oX9GUvRKeFhpqiQps2ungjX_xH4g.JPEG.amytal13/%25EB%25A1%25A0%25EA%25B0%2595%25EC%259D%2598_%25EB%25B3%2584%25EC%259D%25B4%25EB%25B9%259B%25EB%2582%2598%25EB%258A%2594%25EB%25B0%25A4.jpg"
-,
-
-"https://images-ext-1.discordapp.net/external/MVSKMgZPel-HzJ1gFaS8Tb8vSOJ3itd-_-1PdFGLFxE/https/www.picclickimg.com/d/l400/pict/174632906737_/Puzzle-1000-Piezas-Ravensburger-15614-Noche-Estrellada-Sobre.jpg"
-,
-
-"https://images-ext-2.discordapp.net/external/_0ymjNXetQH57BtUs0bSzfAQ8gEYCdUpwejezpLtmsA/https/upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Meisje_met_de_parel.jpg/300px-Meisje_met_de_parel.jpg"
-,
-
-"https://images-ext-2.discordapp.net/external/fZW0LjwR-K_8TCrNNyCGxwMfwZuBWxRvh3Ed0yh_uRU/%3Fq%3Dtbn%3AANd9GcSlpEGOnCW_ew102--j2y_-6O1pYhe-3M7muA%26usqp%3DCAU/https/encrypted-tbn0.gstatic.com/images"
-,
-
-"https://www.corradi.eu/getattachment/d381ade5-acf8-45ae-8ddd-36a616efb8b3/image001-3-.jpg"
-,
-
-"http://artmotion.co.kr/web/upload/NNEditor/20210723/copy-1627014293-860.jpg"
-]
+  "/main/landscape.jpeg",
+  "/main/analysis.gif",
+  "https://www.corradi.eu/getattachment/d381ade5-acf8-45ae-8ddd-36a616efb8b3/image001-3-.jpg",
+  "http://artmotion.co.kr/web/upload/NNEditor/20210723/copy-1627014293-860.jpg",
+];
