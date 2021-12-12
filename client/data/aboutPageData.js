fd2e7e66f9131f45dfc48dd2d88f9e0be6ee8e36
Heeji <limhizy15@ajou.ac.kr>
2021-12-13 00:02:08 +0900

    [#49] refactor: profile 컴포넌트 코드 정리
    * 기능에 영향을 주지 않는 코드 삭제

--- client/data/aboutPageData.js
+++ client/data/aboutPageData.js
@@ -7,16 +7,17 @@ export const backExample =
 export const members = [
   {
     name: "임희지",
-    role: "AI, Frontend",
+    role: "Frontend, AI",
     introduce: [
-      "문제를 해결하기 위해 필요한 기술을 습득하는 걸 좋아합니다.","기술을 빠르게 배우고 적용할 수 있는 힘을 기르고 있습니다. 💪",
+      "문제를 해결하기 위해 필요한 기술을 습득하는 걸 좋아합니다.",
+      "기술을 빠르게 배우고 적용할 수 있는 힘을 기르고 있습니다. 💪",
     ],
     Img: "/images/picture1.jpeg",
     colors: "red blue #D070FB",
   },
   {
     name: "김기원",
-    role: "Backend",
+    role: "Backend, Infra",
     introduce: [
       "Data Scentist로 일 하다가 개발자로 커리어를 전향했습니다.",
       "알고리즘과 운영체제를 좋아해 현재는 알고리즘과 운영체제 공부도 열심히 하고 있습니다.",
@@ -40,7 +41,10 @@ export const members = [
   {
     name: "남은열",
     role: "Frontend",
-    introduce: ["가장 중요한 것은 사용자 경험이라고 생각합니다.", "누구나  사용하기 편하고 좋은 경험을 가질수 있도록 좋은 코드를 작성하는 개발자가 되기위해 하루하루 새로운 지식과 조우하고 있습니다. 🙂"],
+    introduce: [
+      "가장 중요한 것은 사용자 경험이라고 생각합니다.",
+      "누구나  사용하기 편하고 좋은 경험을 가질수 있도록 좋은 코드를 작성하는 개발자가 되기위해 하루하루 새로운 지식과 조우하고 있습니다. 🙂",
+    ],
     Img: "/images/picture4.jpeg",
     colors: "#D070FB",
   },
@@ -48,8 +52,10 @@ export const members = [
     name: "서명현",
     role: "PM",
     introduce: [
-      "1. 하고 싶은 생각 하나,","그리고 그  생각은 언제든지 바뀔 수 있다는 각오 하나.",
-      "2. 주도적인 삶."," 내가 시켜서 하는 나의 일",
+      "1. 하고 싶은 생각 하나,",
+      "그리고 그  생각은 언제든지 바뀔 수 있다는 각오 하나.",
+      "2. 주도적인 삶.",
+      " 내가 시켜서 하는 나의 일",
       "3. 미움 받을 용기",
     ],
     Img: "/images/picture5.png",
@@ -57,7 +63,7 @@ export const members = [
   },
   {
     name: "송성곤",
-    role: "AI, Backend",
+    role: "Backend, AI",
     introduce: [
       "동물, 컴퓨터, 만화를 좋아하고 백엔드 위주로 공부하고 있습니다! 잘 부탁드립니다~",
     ],
@@ -67,7 +73,10 @@ export const members = [
   {
     name: "김현준",
     role: "Backend",
-    introduce: ["하드웨어 엔지니어에서 소프트웨어 엔지니어가 된 김현준입니다."," 개인적인 사유로 끝까지 함께하지는 못하였지만, 멋진 개발자로 다시 뵐 날을 기대하고 있겠습니다."],
+    introduce: [
+      "하드웨어 엔지니어에서 소프트웨어 엔지니어가 된 김현준입니다.",
+      " 개인적인 사유로 끝까지 함께하지는 못하였지만, 멋진 개발자로 다시 뵐 날을 기대하고 있겠습니다.",
+    ],
     Img: "/images/picture7.jpeg",
     colors: "gold",
   },
--- client/src/components/cardbox/Profile.js
+++ client/src/components/cardbox/Profile.js
@@ -1,56 +1,48 @@
 import styled from "styled-components";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import HomeIcon from "@material-ui/icons/Home";
+import { GitHub, Home } from "@material-ui/icons";
 import { Grid } from "@material-ui/core";
 
 export default function Profile({ Img, name, role, introduce, colors }) {
+  console.log(colors);
   return (
-    <Wrapper>
-      <ProfileWrapper>
-        <ProfileNameWrapper>
-          <ProfileImg Img={Img} colors={colors} />
-          <ProfileName>
-            <ProfileLine>{name}</ProfileLine>
-            <ProfileLine>{role}</ProfileLine>
-          </ProfileName>
-        </ProfileNameWrapper>
-        <ProfileCard>
-          {introduce.map((content, index) => (
-            <ProfileContent key ={index}>{content}</ProfileContent>
-          ))}
-        </ProfileCard>
-        <Grid item container spacing={2} justifyContent="center">
-          <Grid
-            item
-            component={"a"}
-            target="_blank"
-            rel="noreferrer noopener"
-            href={"/"}
-          >
-            <HomeIcon color="primary" />
-          </Grid>
-          <Grid
-            item
-            component={"a"}
-            target="_blank"
-            rel="noreferrer noopener"
-            href={"/"}
-          >
-            <GitHubIcon color="primary" />
-          </Grid>
+    <ProfileWrapper>
+      <ProfileNameWrapper>
+        <ProfileImg Img={Img} colors={colors} />
+        <ProfileName>
+          <h2>{name}</h2>
+          <h3>{role}</h3>
+        </ProfileName>
+      </ProfileNameWrapper>
+      <ProfileCard>
+        {introduce.map((content, index) => (
+          <ProfileContent key={index}>{content}</ProfileContent>
+        ))}
+      </ProfileCard>
+      <Grid item container spacing={2} justifyContent="center">
+        <Grid
+          item
+          component={"a"}
+          target="_blank"
+          rel="noreferrer noopener"
+          href={"/"}
+        >
+          <Home color="primary" />
         </Grid>
-      </ProfileWrapper>
-    </Wrapper>
+        <Grid
+          item
+          component={"a"}
+          target="_blank"
+          rel="noreferrer noopener"
+          href={"/"}
+        >
+          <GitHub color="primary" />
+        </Grid>
+      </Grid>
+    </ProfileWrapper>
   );
 }
 
-const Wrapper = styled.div`
-  width: 20vw;
-  display: inline-block;
-  padding-bottom: 8vw;
-`;
-
-const ProfileWrapper = styled.div`
+const ProfileWrapper = styled.section`
   border-radius: 30px;
   width: 20vw;
   height: 60vh;
@@ -59,18 +51,24 @@ const ProfileWrapper = styled.div`
   background-color: white;
   border: solid 4px black;
 `;
+
+const ProfileNameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const ProfileImg = styled.div`
   border-radius: 10vw;
   border: solid 4px;
   border-color: ${(props) => (props.colors ? props.colors : "gray")};
   border-style: inset;
-  width: 9vw;
-  height: 9vw;
+  width: 8vw;
+  height: 8vw;
   margin-left: 1vw;
   margin-top: 1vw;
   overflow: hidden;
   backface-visibility: hidden;
-  position: relative;
   background-color: white;
   background-image: url(${(props) => props.Img});
   background-size: cover;
@@ -78,8 +76,34 @@ const ProfileImg = styled.div`
   z-index: 100;
 `;
 
+const ProfileName = styled.div`
+  width: 10vw;
+  height: 10vw;
+  padding-top: 4vh;
+  overflow: hidden;
+  position: relative;
+  background-color: white;
+  line-height: 2em;
+  font-color: black;
+
+  h2 {
+    padding: 0 0.3em;
+    margin-top: 1vh;
+    line-height: 1.5em;
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: 800;
+  }
+
+  h3 {
+    padding: 0 0.3em;
+    text-align: center;
+  }
+}
+`;
+
 const ProfileCard = styled.div`
-  border-radius: 30px;
+  // border-radius: 30px;
   width: 17vw;
   height: 33vh;
   overflow: hidden;
@@ -92,31 +116,10 @@ const ProfileCard = styled.div`
 
   }
 `;
-const ProfileName = styled.div`
-  width: 10vw;
-  height: 10vw;
-  padding-top:3vw;
-  overflow: hidden;
-  position: relative;
-  background-color: white;
-  line-height: 2em;
-  font-color:black;
-  }
-`;
-
-const ProfileLine = styled.h2`
-  padding: 0.3em;
-  line-height: 1.5em;
-  text-align: center;
-`;
 
 const ProfileContent = styled.h4`
   padding: 0 1vw;
   line-height: 1.2em;
   text-align: left;
   margin: 10px 0;
-`;
-
-const ProfileNameWrapper = styled.div`
-  display: flex;
 `;
