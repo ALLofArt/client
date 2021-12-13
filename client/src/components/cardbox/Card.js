c40219adad20cdbaae4ab5a5747d6539f3479bca
eunsun <nammugmuhg@gmail.com>
2021-12-14 01:55:00 +0900

    [#50]style: 메인 카드 반응형

--- client/src/components/cardbox/Card.js
+++ client/src/components/cardbox/Card.js
@@ -1,37 +1,63 @@
 import styles from "./Card.module.css";
 import styled from "styled-components";
 import { useState } from "react";
-import { useRef } from "react";
-import getRandomColor from "../../../lib/getRandomColor";
+import { Button } from "@material-ui/core";
 
-export default function Card({ frontImg, backImg, explain }) {
+export default function Card({
+  frontImg,
+  backImg,
+  explain,
+  container,
+  next,
+  previous,
+}) {
   const [isRotated, setIsRotated] = useState(false);
   const [clicked, setClicked] = useState(false);
-  const [hovered, setHovered] = useState(false);
   const onRotate = () => setIsRotated((rotated) => !rotated);
-  const word = useRef();
 
   // Converts integer to hex
 
   return (
-    <div className={styles.wrapper}>
-      <div
-        className={`${styles.card} ${isRotated && styles.rotated}`}
-        onClick={onRotate}
-      >
-        <CardImg
-          className={styles.front}
-          Img={frontImg}
-          onClick={() => setClicked(!clicked)}
-        ></CardImg>
+    <Wrapper>
+      <div className={styles.wrapper}>
+        <div
+          className={`${styles.card} ${isRotated && styles.rotated}`}
+          onClick={onRotate}
+        >
+          <CardImg
+            className={styles.front}
+            Img={frontImg}
+            onClick={() => setClicked(!clicked)}
+          ></CardImg>
 
-        {isRotated && (
-          <CardExplain>{explain}</CardExplain>
-        )}
+          {isRotated && <CardExplain>{explain}</CardExplain>}
 
-        <CardImg className={styles.back} Img={backImg} />
+          <CardImg className={styles.back} Img={backImg} />
+        </div>
       </div>
-    </div>
+      <ButtonWrapper>
+        <PreviousButton
+          onClick={() => {
+            container.current.scrollTo({
+              left: previous ? container.current.scrollWidth / previous : 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          Previous
+        </PreviousButton>
+        <NextButton
+          onClick={() => {
+            container.current.scrollTo({
+              left: container.current.scrollWidth / next,
+              behavior: "smooth",
+            });
+          }}
+        >
+          NEXT
+        </NextButton>
+      </ButtonWrapper>
+    </Wrapper>
   );
 }
 
@@ -58,6 +84,47 @@ const CardExplain = styled.strong`
   border-radius: 10px;
   z-index: 2;
   color: white;
-  opacity:0.8;
+  opacity: 0.8;
   transition: background-color 5s linear;
+`;
+const NextButton = styled(Button)`
+  background: black;
+  border-radius: 50px;
+  border: 3px solid black;
+  width: 10vh;
+  height: 5vh;
+  color: white;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  z-index: 2;
+  margin-top: 60vh;
+  display: none;
+  :hover {
+    background: rgba(0, 0, 0, 0.8);
+    border: 3px solid transparent;
+    transform: scale(1.1);
+  }
+
+  span {
+    font-size: 1.5vh;
+    font-weight: 800;
+  }
+  @media only screen and (max-width: 45rem) {
+    display: block;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const PreviousButton = styled(NextButton)``;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 1vh;
 `;
